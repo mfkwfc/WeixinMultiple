365a61cbfd57652887c45754ce47b2c797fddb48
porridge <mfkwfc@gmail.com>
2018-06-06 18:46:43 +0800

    first commit

---
+++ src/main/java/porridge/WeixinMultiple/WeixinMultipleApplication.java
@@ -0,0 +1,12 @@
+package porridge.WeixinMultiple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeixinMultipleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeixinMultipleApplication.class, args);
+	}
+}
